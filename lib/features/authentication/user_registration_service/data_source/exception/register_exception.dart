1b99fd6f8a7549342fc785e8c66a4bd4da796ed3
Riadh <gharbiriadh16@gmail.com>
2025-01-29 11:44:40 +0100

    feat(authentication): Improve registration exception handling
    - Introduced a `RegistrationException` class as a base for registration-related exceptions.
    - Added an `UnknownRegistrationException` to handle cases where the cause of registration failure is undetermined.
    - Updated `DataSavingException` to inherit from `ApiException`.
    - Added the documentation to all exception.
    - This improves the clarity and specificity of error handling during user registration.

--- lib/features/authentication/user_registration_service/data_source/exception/register_exception.dart
+++ lib/features/authentication/user_registration_service/data_source/exception/register_exception.dart
@@ -1,11 +1,27 @@
 import 'package:deliverapi/api_exception.dart';
 
-/// Exception for registration failures.
+/// Represents an exception that occurs during the user registration process.
+///
+/// This class serves as a base for specific registration-related exceptions.
 class RegistrationException extends ApiException {
+  /// Creates a new [RegistrationException] with the specified [message].
   RegistrationException(super.message);
 }
 
-/// Exception for data saving failures.
+/// Represents an exception that occurs when a user registration fails due to an unknown reason.
+///
+/// This exception is thrown when the specific cause of the registration failure
+/// cannot be determined.
+class UnknownRegistrationException extends RegistrationException {
+  /// Creates a new [UnknownRegistrationException] with the specified [message].
+  UnknownRegistrationException(super.message);
+}
+
+/// Represents an exception that occurs when there's a failure while saving data during user registration.
+///
+/// This exception is thrown when the application is unable to persist the user's
+/// registration data, such as user details or preferences, to a data store.
 class DataSavingException extends ApiException {
+  /// Creates a new [DataSavingException] with the specified [message].
   DataSavingException(super.message);
 }
